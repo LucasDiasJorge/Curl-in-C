30f5b0f89a391b0c07d31ae724ed667aa2a1f7be
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-06-11 22:44:34 -0300

    Making get as a function and argv bug fixed

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <curl/curl.h>
  
 size_t got_data(char *buffer, size_t itemsize, size_t nitems, void* ignorethis){
@@ -30,22 +31,13 @@ void getError(CURLcode res) {
   fprintf(stderr, "Error: %s\n", curl_easy_strerror(res));
 }
 
-int main(int argc, char *argv[]){
-  CURL *curl;
-  CURLcode res;
- 
-  curl = curl_easy_init();
-  
-  char operation = *argv[1];
+void httpGet(CURL *curl, CURLcode res){
 
-  //Get
-  if(curl && operation == 'g') {
-
-    struct curl_slist *headers = NULL;
+  struct curl_slist *headers = NULL;
 
     //headers = curl_slist_append(headers, "Authorization: Bearer <TOKEN>");
 
-    curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.1.65:8888/api/v1/ping");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8888/api/v1/ping");
     //curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, FALSE);
     curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
@@ -60,6 +52,28 @@ int main(int argc, char *argv[]){
 
     curl_slist_free_all(headers);
     curl_easy_cleanup(curl);
+
+}
+
+
+int main(int argc, char *argv[]){
+  CURL *curl;
+  CURLcode res;
+ 
+  curl = curl_easy_init();
+  
+  if(argv[1] == NULL){
+    perror("Operation not found");
+    exit(EXIT_FAILURE);
+  }
+
+  char operation = *argv[1];
+
+  //Get
+  if(curl && operation == 'g') {
+
+    httpGet(curl,res);
+    
   }
 
   //Post
@@ -71,7 +85,7 @@ int main(int argc, char *argv[]){
 
     //headers = curl_slist_append(headers, "Authorization: Bearer <TOKEN>");
 
-    curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.1.65:8888/api/v1/auth");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8888/api/v1/auth");
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "{\"user\":\"email@email.com\",\"pass\":\"easypass\"}");
     curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
@@ -94,7 +108,7 @@ int main(int argc, char *argv[]){
 
     //headers = curl_slist_append(headers, "Authorization: Bearer <TOKEN>");
 
-    curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.1.65:8888/api/v1/delete?id=3");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8888/api/v1/delete?id=3");
     curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST,"DELETE");
     curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
@@ -119,7 +133,7 @@ int main(int argc, char *argv[]){
 
     //headers = curl_slist_append(headers, "Authorization: Bearer <TOKEN>");
 
-    curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.1.65:8888/api/v1/update");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8888/api/v1/update");
     curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "{\"key\":\"value\"}");
     curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
