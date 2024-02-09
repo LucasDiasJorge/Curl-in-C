e6404679c7bb2026a4080d93e33760760329247f
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-02-09 15:51:34 -0300

    Refactoring

--- Simple-lib/http-request.c
+++ Simple-lib/http-request.c
@@ -3,12 +3,19 @@
 #include <curl/curl.h>
 
 #define MAX_RESPONSE_SIZE 1024
+#define USERNAME "root"
+#define PASSWORD "root"
 
 char response[MAX_RESPONSE_SIZE];
 
 size_t write_callback(void *ptr, size_t size, size_t nmemb, void *userdata) {
     size_t total_size = size * nmemb;
-    strcat((char*)userdata, (char*)ptr);
+    size_t buffer_size = strlen((char*)userdata);
+    if (buffer_size + total_size < MAX_RESPONSE_SIZE) {
+        strcat((char*)userdata, (char*)ptr);
+    } else {
+        fprintf(stderr, "Response buffer overflow!\n");
+    }
     return total_size;
 }
 
@@ -25,7 +32,7 @@ int send_curl_request(const char *method, const char *url, char *response_ref) {
         curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method);
 
         // Set the username and password for basic authentication
-        curl_easy_setopt(curl, CURLOPT_USERPWD, "root:root");
+        curl_easy_setopt(curl, CURLOPT_USERPWD, USERNAME ":" PASSWORD);
 
         // Set the write callback function to handle the response
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
@@ -41,18 +48,12 @@ int send_curl_request(const char *method, const char *url, char *response_ref) {
         if(res != CURLE_OK) {
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
             curl_easy_cleanup(curl);
-            printf("\n");
             return 1;
         }
-
-        // Copy the response to the provided buffer
-        strcpy(response_ref, response);
 
         // Cleanup
         curl_easy_cleanup(curl);
     }
-
-    printf("\n");
 
     return 0;
 }
